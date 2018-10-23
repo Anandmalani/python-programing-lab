0d6a987d8c2812e1ec069daff823fb03b0ecb0b8
Anandmalani <43986351+Anandmalani@users.noreply.github.com>
2018-10-23 16:08:19 +0530

    Add files via upload

--- anand.py
+++ anand.py
@@ -1,15 +1,18 @@
+#ANAND MALANI
+#ROLL NO. 37
+#GR NO.  11810791
 
 
 from easygui import *
 import sys
-
-
+list1=[]
+sum =0
 while 1:
     msgbox("Hello, India!")
 
     msg ="choose shopping site according to you"
     title = "shopping purpose"
-    choices = ["amazon","flipcard","mantra"]
+    choices = ["amazon","flipcard"]
     choice = choicebox(msg, title, choices)
     msgbox("You chose: " + str(choice), "Survey Result")
     if choice=="amazon":
@@ -19,14 +22,44 @@ while 1:
        choices1=["shoes","laptop"]
        choice1=choicebox(msg,title,choices1)
        if choice1=="shoes":
-           msgbox("shoes price")
-       else:
-           msgbox("laptop price")
+           m ="shoes price = 2000\n"
+           msgbox(m)
+	   list1.append(m)
+	   sum += 2000
+           #msgbox("total price= 2000")
+           
+       elif choice1=="laptop":
+            n="laptop price=35000\n"
+            msgbox(n)
+            list1.append(n)
+            sum += 35000 
+           #sgbox("laptop price=35000") 
+           
+    else:
+       msgbox("hello flipcard")
+       msg ="choose product"
+       title="product detail"
+       choices2=["laptop","mobile"]
+       choice2=choicebox(msg,title,choices2)
+       if choice2=='mobile':
+           p="mobile price = 3000\n"
+           msgbox(p)
+           list1.append(p)
+           sum += 3000
+           #msgbox("total price= 3000")
+
+       elif choice2=="laptop":
+            u="laptop price=35000\n"
+            msgbox(u)
+            list1.append(u)
+            sum += 35000 
+           #sgbox("laptop price=35000")
     msg = "Do you want to continue?"
     title = "Please Confirm"
     if ccbox(msg, title):     # show a Continue/Cancel dialog
         pass  # user chose Continue
     else:
+	textbox('Purchase memo', 'Bill Desk',list1)
         sys.exit(0)
 
 
