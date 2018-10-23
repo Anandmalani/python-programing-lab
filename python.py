65ad99c51777a2a7ca0fbe7a71985ad1584e008d
Anandmalani <43986351+Anandmalani@users.noreply.github.com>
2018-10-23 16:57:16 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,31 @@
+#ANAND MALANI
+#ROLL NO. 37
+#GR NO.  11810791
+
+def factorial(x):
+   if x<0:
+     print("enter positive no")
+   elif x==0:
+     print(" factorial of 0 is 1")
+   else:
+     f=1
+     while x>o:
+       f=f*x
+       x=x-1
+factorial(3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
