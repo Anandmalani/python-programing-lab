e9b92efa0a24b97dd1003e9dcd9cfff6a5891da7
Anandmalani <43986351+Anandmalani@users.noreply.github.com>
2018-10-23 23:59:30 +0530

    Update arm.py

--- arm.py
+++ arm.py
@@ -1,13 +1,18 @@
-#Angish bhandwalkar 11810766 M03
-x=int(input())
-a=x
-sum=0
-while a>0:
+#Anand Malani 
+#11810791 M37
+def arm(x):
+   a=x
+   sum=0
+   while a>0:
     n=a%10
     sum=sum+n**3
     a=a//10
-print(sum)
-if sum==x:
+   print(sum)
+   if sum==x:
        print("it is a arm")
-else :
+   else :
     print("it is not a arm")
+   while 1:
+    x=int(input())
+    arm(X)
+    
