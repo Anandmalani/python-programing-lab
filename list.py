1528efeb52f42ad183ccc3b864ffabf367d8ff43
Anandmalani <43986351+Anandmalani@users.noreply.github.com>
2018-10-29 17:01:44 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,15 @@
+#anand malani
+#roll no 37
+#gr no 11181091
+
+n1=input("enter the name")        #enter the name
+e1=int(input())    #enter the marks of english
+m1=int(input())   #enter the marks of maths
+n2=input("enter the name")        #enter the name 
+e2=int(input())    #enter the marks of english
+m2=int(input())   #enter the marks of maths
+l=["n1","eng1",e1,"mat1",m1]
+m=["n2","eng2",e2,"mat2",m2]
+x=[l,m]
+
+
