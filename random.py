835811556d217b0562505f62e80e0b5f1dfcd803
Anandmalani <43986351+Anandmalani@users.noreply.github.com>
2018-10-30 14:17:58 +0530

    Add files via upload

---
+++ random.py
@@ -0,0 +1,11 @@
+
+#   Anand malani 11810791 M 37
+import random
+from random import randint as rt
+def game(dice,faces):
+    result=0
+    for roll in range(0,dice):
+        result+=rt(1,faces)
+        return result
+result=game(1,6)
+print(result)
