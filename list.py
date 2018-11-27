b0122c2ac8df7eaf7d9f8cce0a36847428717180
Anandmalani <43986351+Anandmalani@users.noreply.github.com>
2018-11-27 14:55:26 +0530

    Update list.py

--- list.py
+++ list.py
@@ -8,8 +8,9 @@ m1=int(input())   #enter the marks of maths
 n2=input("enter the name")        #enter the name 
 e2=int(input())    #enter the marks of english
 m2=int(input())   #enter the marks of maths
-l=["n1","eng1",e1,"mat1",m1]
-m=["n2","eng2",e2,"mat2",m2]
+l=[n1,"eng1",e1,"mat1",m1]
+m=[n2,"eng2",e2,"mat2",m2]
 x=[l,m]
+print(x)
 
 
